8c549bde6c35a804251088f58371f671b755ca1c
bertanceviren <bertan@pop-os.localdomain>
2020-03-19 20:04:18 -0400

    master adding

--- src/GitTest/Git01.java
+++ src/GitTest/Git01.java
@@ -4,5 +4,6 @@ public class Git01 {
 public static void main(String[] args) {
 	System.out.println("hello Git 01");
 	System.out.println("Hello git branch");
+	System.out.println("Hello git master branch");
 }
 }
