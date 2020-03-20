9ca954eea3dc6ffa50960b584316cb2ff6c1b317
bertanceviren <bertan@pop-os.localdomain>
2020-03-19 20:44:01 -0400

    check this out

--- src/GitTest/Git01.java
+++ src/GitTest/Git01.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("hello Git 01");
 	System.out.println("Hello git branch");
 	System.out.println("Hello git master branch");
+	System.out.println("hey check this out");
 }
 }
